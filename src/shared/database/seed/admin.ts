64a063cadfbe26eca4f08f0f33cd9c36520a9d84
Gleyson Abreu <gleyson_datu@hotmail.com>
2021-05-25 20:14:19 -0300

    Add seed admin

---
+++ src/shared/database/seed/admin.ts
@@ -0,0 +1,26 @@
+import { v4 as uuid } from 'uuid';
+
+import { hash } from '@shared/services/password';
+
+import connect from '../connection';
+
+async function createAdmin() {
+  const connection = await connect();
+
+  const id = uuid();
+  const idPermission = uuid();
+  const permission = 1;
+  const password = await hash('admin');
+
+  await connection.query(
+    `INSERT INTO users (id, first_name, last_name, email, password, cpf, birth_date, phone) VALUES('${id}', 'Admin', 'Admin', 'admin@admin.com', '${password}', '00000000000', '02-16-1998', '00000000000')`,
+  );
+
+  await connection.query(
+    `INSERT INTO permissions (id, user_id, type) VALUES ('${idPermission}', '${id}', '${permission}')`,
+  );
+
+  await connection.close();
+}
+
+createAdmin().then(() => console.log('User admin created!'));
