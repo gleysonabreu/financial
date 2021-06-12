964235bd3add5f9c4945409f2ecf1d4d9471e10a
Gleyson Abreu <gleyson_datu@hotmail.com>
2021-06-12 15:53:18 -0300

    test: add setup to integration tests

--- src/config/dotEnv.ts
+++ src/config/dotEnv.ts
@@ -1,5 +1,23 @@
 import dotenv from 'dotenv';
+import { resolve } from 'path';
+
+const envFile = (environment: string) => {
+  let file: string;
+
+  switch (environment) {
+    case 'test':
+      file = '.env.test';
+      break;
+    case 'dev':
+      file = '.env.dev';
+      break;
+    default:
+      file = '.env';
+  }
+
+  return file;
+};
 
 dotenv.config({
-  path: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env',
+  path: resolve(__dirname, '..', '..', envFile(process.env.NODE_ENV)),
 });
