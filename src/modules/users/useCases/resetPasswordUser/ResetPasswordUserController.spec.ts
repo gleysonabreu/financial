05d5ff0e29ec608804c2ce507cf768e199cf9fee
Gleyson Abreu <gleyson_datu@hotmail.com>
2021-06-14 12:09:31 -0300

    test: add integration test to ResetPasswordUserController

---
+++ src/modules/users/useCases/resetPasswordUser/ResetPasswordUserController.spec.ts
@@ -0,0 +1,54 @@
+import { permissions } from '@config/permissions';
+import request from 'supertest';
+import { Connection } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+import createConnection from '@shared/database/connection';
+import { app } from '@shared/infra/http/app';
+import { hash } from '@shared/services/password';
+
+let connection: Connection;
+describe('ResetPasswordUserController', () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const id = uuid();
+    const idPermission = uuid();
+    const password = await hash('admin123');
+
+    await connection.query(
+      `INSERT INTO users (id, first_name, last_name, email, password, cpf, birth_date, phone) VALUES('${id}', 'Admin', 'Admin', 'admin@admin.com', '${password}', '00000000000', '02-16-1998', '00000000000')`,
+    );
+
+    await connection.query(
+      `INSERT INTO permissions (id, user_id, type) VALUES ('${idPermission}', '${id}', '${permissions.ADMIN}')`,
+    );
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it('should not be able to reset password if token does not exists', async () => {
+    const response = await request(app)
+      .post('/password/reset')
+      .query({
+        token: uuid(),
+      })
+      .send({
+        password: '1234567',
+      });
+
+    expect(response.status).toBe(400);
+  });
+
+  it('should not be able to reset password with missing password', async () => {
+    const response = await request(app).post('/password/reset').query({
+      token: uuid(),
+    });
+
+    expect(response.status).toBe(400);
+  });
+});
