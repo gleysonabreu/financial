2cc33345975854617681272c42d162c5b19a98dd
Gleyson Abreu <gleyson_datu@hotmail.com>
2021-06-14 18:04:53 -0300

    test: add integration test to CreateAccountTypeController

---
+++ src/modules/accountTypes/useCases/createAccountType/CreateAccountTypeController.spec.ts
@@ -0,0 +1,92 @@
+import { permissions } from '@config/permissions';
+import request from 'supertest';
+import { Connection } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+import createConnection from '@shared/database/connection';
+import { app } from '@shared/infra/http/app';
+import { hash } from '@shared/services/password';
+
+let connection: Connection;
+describe('CreateAccountTypeController', () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const id = uuid();
+    const idPermission = uuid();
+    const password = await hash('admin123');
+
+    await connection.query(
+      `INSERT INTO users (id, first_name, last_name, email, password, cpf, birth_date, phone) VALUES('${id}', 'Admin', 'Admin', 'admin@admin.com', '${password}', '00000000000', '02-16-1998', '00000000000')`,
+    );
+
+    await connection.query(
+      `INSERT INTO permissions (id, user_id, type) VALUES ('${idPermission}', '${id}', '${permissions.ADMIN}')`,
+    );
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it('should be able to create an account type', async () => {
+    const { token } = (
+      await request(app).post('/auth').send({
+        email: 'admin@admin.com',
+        password: 'admin123',
+      })
+    ).body;
+
+    const response = await request(app)
+      .post('/account-types')
+      .send({
+        name: 'test',
+      })
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(204);
+  });
+
+  it('should not be able to create an account type without authenticate', async () => {
+    const response = await request(app).post('/account-types').send({
+      name: 'test',
+    });
+
+    expect(response.status).toBe(401);
+  });
+
+  it('should not be able to create an account type if name already exists', async () => {
+    const { token } = (
+      await request(app).post('/auth').send({
+        email: 'admin@admin.com',
+        password: 'admin123',
+      })
+    ).body;
+
+    const response = await request(app)
+      .post('/account-types')
+      .send({
+        name: 'test',
+      })
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(400);
+  });
+
+  it('should not be able to create an account type with missing fields', async () => {
+    const { token } = (
+      await request(app).post('/auth').send({
+        email: 'admin@admin.com',
+        password: 'admin123',
+      })
+    ).body;
+
+    const response = await request(app)
+      .post('/account-types')
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(400);
+  });
+});
