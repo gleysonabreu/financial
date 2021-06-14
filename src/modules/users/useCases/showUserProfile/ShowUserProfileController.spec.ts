b4aa6659e32df73a1f43efc5497dad42c2571f04
Gleyson Abreu <gleyson_datu@hotmail.com>
2021-06-14 10:17:37 -0300

    test: add integration test to ShowUserProfile and GetOneUser

---
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.spec.ts
@@ -0,0 +1,56 @@
+import { permissions } from '@config/permissions';
+import { UserToken } from '@modules/users/entities/UserToken';
+import request from 'supertest';
+import { Connection } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+import createConnection from '@shared/database/connection';
+import { app } from '@shared/infra/http/app';
+import { hash } from '@shared/services/password';
+
+let connection: Connection;
+describe('ShowUserProfileController', () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const id = uuid();
+    const idPermission = uuid();
+    const password = await hash('admin123');
+
+    await connection.query(
+      `INSERT INTO users (id, first_name, last_name, email, password, cpf, birth_date, phone) VALUES('${id}', 'Admin', 'Admin', 'admin@admin.com', '${password}', '00000000000', '02-16-1998', '00000000000')`,
+    );
+
+    await connection.query(
+      `INSERT INTO permissions (id, user_id, type) VALUES ('${idPermission}', '${id}', '${permissions.ADMIN}')`,
+    );
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it('should be able to get profile', async () => {
+    const { token, user } = (
+      await request(app).post('/auth').send({
+        email: 'admin@admin.com',
+        password: 'admin123',
+      })
+    ).body;
+
+    const response = await request(app)
+      .get(`/profile`)
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(200);
+    expect(response.body.id).toEqual(user.id);
+  });
+
+  it('should not be able to get profile without authenticate', async () => {
+    const response = await request(app).get(`/profile`);
+
+    expect(response.status).toBe(401);
+  });
+});
